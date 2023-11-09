c51fdebcebaeb92ad46a9e240467aa649b3e04de
OIA <ovsyannikovigora@yandex.ru>
2023-11-09 21:15:21 +0300

    Homework for the 2nd lesson FIX

--- test_home_working.py
+++ test_home_working.py
@@ -1,15 +1,18 @@
+import time
+
 import pytest
 from selene import be, have
 from selene.support.shared import browser
 
-browser.config.timeout = 10
+
 @pytest.fixture()
 def size_window():
-    browser.driver.set_window_size(1920, 1080)
-    yield
     browser.driver.set_window_size(720, 480)
+    yield
+    browser.driver.set_window_size(1920, 1080)
     browser.open('https://animals.pibig.info/uploads/posts/2023-04/'
                  '1681420564_animals-pibig-info-p-koti-s-yazikom-zhivotnie-vkontakte-1.jpg')
+    time.sleep(5)
 
 
 def test_search_nothing(size_window):
